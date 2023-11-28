0532aae1ca7bc870004951a6cb027e3d0ab41a27
SandroRDS <sandror.desouza847@gmail.com>
2023-11-27 23:37:53 -0300

    Removendo conteúdo do Front-End

--- src/main.tsx
+++ src/main.tsx
@@ -6,20 +6,3 @@ import './reset.css';
 // import TipsGenerator from './adapters/TipsGenerator.ts';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
-// const tipsGenerator = new TipsGenerator();
-// tipsGenerator.requestChatGPT();
-
-import OpenAI from "openai";
-
-const openai = new OpenAI({ apiKey: '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', dangerouslyAllowBrowser: true });
-
-async function main() {
-  const completion = await openai.chat.completions.create({
-    messages: [{ role: "system", content: "You are a helpful assistant." }],
-    model: "gpt-3.5-turbo",
-  });
-
-  console.log(completion.choices[0]);
-}
-
-main();
